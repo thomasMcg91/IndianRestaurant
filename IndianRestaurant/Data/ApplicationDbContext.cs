b77a5a148b4b2900369eb36272bd771e373cb58c
Thomas <mcgthomas@hotmail.com>
2019-10-21 16:31:17 +0100

    Adding menuItem model and updating database

--- IndianRestaurant/Data/ApplicationDbContext.cs
+++ IndianRestaurant/Data/ApplicationDbContext.cs
@@ -15,5 +15,7 @@ namespace IndianRestaurant.Data
         }
         public DbSet<Category> Category { get; set; }
         public DbSet<SubCategory> SubCategory { get; set; }
+
+        public DbSet<MenuItem> MenuItem { get; set; }
     }
 }
---
+++ IndianRestaurant/Models/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndianRestaurant.Models
+{
+    public class MenuItem
+    {
+
+        //need to alter casace to noaction in migration that if a category or sub is deleted, it will not delete the menuitem table
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Spicyness { get; set; }
+
+        public enum ESpicy { NA = 0, NotSpicy = 1, Spicy = 2, VerySpicy = 3 }
+
+        public string Image { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { set; get; }
+
+
+        [Display(Name = "SubCategory")]
+        public int SubCategoryId { get; set; }
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { set; get; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price should be greater than ${1}")]
+        public double Price { get; set; }
+
+    }
+}
